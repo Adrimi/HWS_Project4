d53e5ff49fad0ecb693c05a15dadda14e6c8b23f
Adrimi <adriano100417@gmail.com>
2019-03-20 11:23:58 +0100

    Init commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Adrimi on 20/03/2019.
+//  Copyright © 2019 Adrimi. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
